f89c8efa5abc1c79e3b046f8653d9c9f38c256cc
David Burkhart <dburkhart77@gmail.com>
2023-03-19 18:53:59 -0600

    wtf

--- db/index.js
+++ db/index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const { Client } = require("pg");
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL);
 
 // -----USER METHODS-----
 // -----CREATE A USER-----
